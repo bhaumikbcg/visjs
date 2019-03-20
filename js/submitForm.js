91931de7f0af6ba31a131b3c9a8afb376e3161b7
bhaumikbcg <bhaumikbcg@yahoo.com>
2019-03-20 14:43:58 -0700

    Made some UI changes for buttons. Grouped buttons togeher. Created a new folder for all javascript files.

---
+++ js/submitForm.js
@@ -0,0 +1,10 @@
+function submitForm(){  
+    var frm = document.getElementsByName('upload-form')[0];
+    frm.submit(); // Submit the form
+    frm.reset();  // Reset all form data
+    functionConfirm("Do you like Football?", function yes() {   
+    }, function no() {
+        alert("no")
+    });
+    return false; // Prevent page refresh
+}
