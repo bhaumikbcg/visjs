bc9494229e5f9b309d0b5b64cc905d0babdb334b
bhaumikbcg <bhaumikbcg@yahoo.com>
2019-03-26 12:56:31 -0700

    The hierarchy on the left side is now working

--- js/getHierarchy.js
+++ js/getHierarchy.js
@@ -1,14 +1,10 @@
-var nh_name = [];
-var nh_id = [];
-var nh_level = [];
 var response;
 var xhr;
 var neighbourhoodName = "root";
 var neighbourhoodId = -1;
-var level0 = {};
-var level1 = {};
-var level2 = {};
-var level3 = {};
+var nhName;
+var nhNameData = {};
+nhNameData.id = -10;
 var url = 'http://192.168.250.23:5000/expand?expand_type=root&expand_id=-1';
 $(getHierarchy(neighbourhoodName, neighbourhoodId));
 function getHierarchy(neighbourhoodName, neighbourhoodId){
@@ -22,16 +18,37 @@ function getHierarchy(neighbourhoodName, neighbourhoodId){
             passResponse(response);
         }
     }
-    function passResponse(myResponse){
-        for(var i = 0; i<myResponse.length; i++){
-            getHierarchy('nh_level', myResponse[i][1][1]);
-            // nh_name.push(myResponse[i][2][1]);
-            // nh_id.push(myResponse[i][1][1]);
-            // nh_level.push(myResponse[i][0][1]);
-            // level0.name = nh_name;
-            // level0.id = nh_id;
-            // level0.level = nh_level;
-        }
-        console.dir(level0);
+}
+// function passResponse(myResponse){
+//     for(var i = 0; i<myResponse.length; i++){
+//         if(myResponse[i][2][1] === "ROOT" || nhNameData.id === myResponse[i][1][1]) continue;
+//         var myObject = {};
+//         nhName = myResponse[i][2][1];
+//         myObject.name = nhName;
+//         myObject.id = myResponse[i][1][1];
+//         myObject.level = myResponse[i][0][1];
+//         localStorage.setItem(nhName, JSON.stringify(myObject));
+//         if(myResponse[i][0][1] === 0)
+//             $('#hierarchy').append('<li id="' + myResponse[i][2][1] + '"><button type="button" class="btn btn-link" onclick="goInHierarchy(this)">'+myResponse[i][2][1]+'</button></li>');
+//         else
+//             $('#' + nhNameData.name).append('<ul><li id="' + myResponse[i][2][1] + '"><button type="button" class="btn btn-link" onclick="goInHierarchy(this)">'+myResponse[i][2][1]+'</button></li></ul>');
+//     }
+// }
+
+
+function passResponse(myResponse){
+    for(var i = 0; i<myResponse.length; i++){
+        if(myResponse[i][2][1] === "ROOT" || nhNameData.id === myResponse[i][1][1]) continue;
+        var myObject = {};
+        nhName = myResponse[i][2][1];
+        myObject.name = nhName;
+        myObject.id = myResponse[i][1][1];
+        myObject.level = myResponse[i][0][1];
+        myObject.count = -1;
+        localStorage.setItem(nhName, JSON.stringify(myObject));
+        if(myResponse[i][0][1] === 0)
+            $('#hierarchy').append('<li><button type="button" class="btn btn-link" data-toggle="collapse" data-target="#' + myResponse[i][2][1] +'" aria-expanded="false" aria-controls="collapseExample" onclick="goInHierarchy(this)">'+myResponse[i][2][1]+'</button></li><div class="collpase" id="' + myResponse[i][2][1] +'"></div>');
+        else
+            $('#' + nhNameData.name).append('<ul><li><button type="button" class="btn btn-link" data-toggle="collapse" data-target="#' + myResponse[i][2][1] +'" aria-expanded="false" aria-controls="collapseExample" onclick="goInHierarchy(this)">'+myResponse[i][2][1]+'</button></li><div class="collpase" id="' + myResponse[i][2][1] +'"></div></ul>');
     }
 }
---
+++ js/goInHierarchy.js
@@ -0,0 +1,13 @@
+function goInHierarchy(a){
+    var receivedNhName = a.innerHTML;
+    nhNameData = JSON.parse(localStorage.getItem(receivedNhName));
+    nhNameData.count += 1;
+    localStorage.setItem(receivedNhName, JSON.stringify(nhNameData));
+    nhNameData = JSON.parse(localStorage.getItem(receivedNhName));
+    console.dir(nhNameData);
+    if(nhNameData.count === 0){
+        neighbourhoodName = "nh_level";
+        neighbourhoodId = nhNameData.id;
+        getHierarchy(neighbourhoodName, neighbourhoodId);
+    }
+}
