f433f286f72ece62498645ebd1a31f4e07849ddc
bhaumikbcg <bhaumikbcg@yahoo.com>
2019-06-11 16:46:49 -0700

    Upto cuboid search working properly

--- js/containsObject.js
+++ js/containsObject.js
@@ -1,14 +1,13 @@
-// function containsObject(obj, list){
-//     list.forEach(element => {
-//         if(obj.from === element.from && obj.to === element.to) return true;
-//     });
-//     return false;
-// }
-
-
 function containsObject(obj, list){
     for(var i = 0; i < list.length; i++){
         if(obj.from === list[i].from && obj.to === list[i].to) return true;
     }
     return false;
+}
+
+function containsObject1(individualEdgeObject, edgeData){
+    for(var i = 0; i < edgeData.length; i++){
+        if(individualEdgeObject.from === edgeData[i].from && individualEdgeObject.to === edgeData[i].to) break;
+    }
+    edgeData.push(individualEdgeObject);
 }
